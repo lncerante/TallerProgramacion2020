f4ad4025bc700c572c477bfb97e6f1ca91d46e77
rebebenedetti <rebebenedetti@gmail.com>
2023-07-05 16:01:20 -0300

    feat: comentarios en Context.

--- Context/WinFormsContext.cs
+++ Context/WinFormsContext.cs
@@ -20,12 +20,21 @@ namespace TallerProgramacion2020.WinFormsContextClass
 
         protected IEnumerable<MediaDTO> iCurrentMedia;
 
+        /// <summary>
+        /// Clase que actúa como contexto en una aplicación de Windows Forms.
+        /// Permite almacenar información compartida y acceder a ella desde diferentes partes de la aplicación.
+        /// </summary>
         protected WinFormsContext()
         {
             iUser = null;
             iRootForm = null;
         }
 
+        /// <summary>
+        /// Permite acceder al contexto de la aplicación en diferentes partes del código.
+        /// </summary>
+        /// <returns>Retorna una instancia única de la clase "WinFormsContext". 
+        /// Si no existe una instancia previa, crea una nueva instancia y la retorna.</returns>
         public static WinFormsContext GetInstance()
         {
             if (_instance == null)
@@ -35,6 +44,10 @@ namespace TallerProgramacion2020.WinFormsContextClass
             return _instance;
         }
 
+        /// <summary>
+        /// Reinicia el contexto de la aplicación, creando una nueva instancia de la clase "WinFormsContext".
+        /// </summary>
+        /// <returns>Si no existe una instancia previa, crea una nueva instancia y la retorna</returns>
         public static WinFormsContext Reset()
         {
             if (_instance == null)
@@ -44,18 +57,27 @@ namespace TallerProgramacion2020.WinFormsContextClass
             return _instance;
         }
 
+        /// <summary>
+        /// Permite acceder al objeto UserDTO que representa al usuario actual en el contexto.
+        /// </summary>
         public UserDTO User
         {
             get { return iUser; }
             set { iUser = value; }
         }
 
+        /// <summary>
+        /// Permite acceder al formulario raíz de la aplicación en el contexto.
+        /// </summary>
         public FormMenu RootForm
         {
             get { return iRootForm; }
             set { iRootForm = value; }
         }
 
+        /// <summary>
+        /// Permite acceder a la colección de objetos MediaDTO que representa las peliculas y series actuales en el contexto.
+        /// </summary>
         public IEnumerable<MediaDTO> CurrentMedia
         {
             get { return iCurrentMedia; }
