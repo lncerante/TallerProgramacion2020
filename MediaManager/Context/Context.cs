2945c1383cc5f47ab9c60872a734c6dfe9d4b3ec
rebebenedetti <rebebenedetti@gmail.com>
2023-07-06 13:02:52 -0300

    feat: comentarios en MediaManager/Context

--- MediaManager/Context/Context.cs
+++ MediaManager/Context/Context.cs
@@ -10,6 +10,9 @@ using TallerProgramacion2020.MediaManager.Domain;
 
 namespace TallerProgramacion2020.MediaManager.ContextClass
 {
+    /// <summary>
+    /// Clase que representa el contexto de la aplicación.
+    /// </summary>
     public class Context
     {
         protected static Context _instance;
@@ -18,12 +21,19 @@ namespace TallerProgramacion2020.MediaManager.ContextClass
 
         protected User iUser;
 
+        /// <summary>
+        /// Constructor protegido de la clase Context.
+        /// </summary>
         protected Context()
         {
             iUnitOfWork = new UnitOfWork(new MediaManagerDbContext());
             iUser = null;
         }
 
+        /// <summary>
+        /// Método estático para obtener una instancia del contexto.
+        /// </summary>
+        /// <returns>Instancia del contexto.</returns>
         public static Context GetInstance()
         {
             if (_instance == null)
@@ -33,11 +43,17 @@ namespace TallerProgramacion2020.MediaManager.ContextClass
             return _instance;
         }
 
+        /// <summary>
+        /// Unidad de trabajo asociada al contexto.
+        /// </summary>
         public IUnitOfWork UnitOfWork
         {
             get { return iUnitOfWork; }
         }
 
+        /// <summary>
+        /// Usuario asociado al contexto.
+        /// </summary>
         public User User
         {
             get { return iUser; }
